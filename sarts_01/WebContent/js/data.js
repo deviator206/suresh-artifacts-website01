efb26625be6314767ca6d0960a1531bd7db9bc19
unknown <deshpad1@RSHMW12607.asia.roche.com>
2014-12-21 17:12:27 +0530

    added files provided by suresh

---
+++ sarts_01/WebContent/js/data.js
@@ -0,0 +1,198 @@
+
+var categoryArray = [ {
+	categoryId : "cat1",
+	categoryName : "Painting-1",
+	defaultName : "Default Painting",
+	categoryDivName : "idGalleryItemsContainer",
+	collapseOne : "collapseOne-1",
+	categorySource : [ {
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_1.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	}, {
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_2.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},
+
+	
+	{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_3.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_4.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_5.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_6.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_7.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_8.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_9.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_10.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_11.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_12.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_13.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_14.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_15.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_16.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_17.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_18.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},
+	]
+}, {
+	categoryId : "cat2",
+	categoryName : "Painting-2",
+	defaultName : "Default Painting",
+	categoryDivName : "idGalleryItemsContainer-Painting",
+	collapseOne : "collapseOne-2",
+	categorySource : [ {
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_19.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	}, {
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_20.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},
+	
+	
+	
+	{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_21.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},
+	{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_22.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_23.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_24.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_25.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_26.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_27.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_28.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_29.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_30.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_31.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_32.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_33.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},{
+		"imgPath" : "img/thumbnails/Thumbnail_211x141_34.jpg",
+		"imgDescription" : "Thumbnail 1",
+		"imgDimension" : "10x10x01x10",
+		"imgPrice" : "2000INR"
+	},
+	
+
+	]
+} ]
--- sarts_01/WebContent/js/src.js
+++ sarts_01/WebContent/js/src.js
@@ -1,118 +1,210 @@
 $(document)
-	.ready(function() {
+		.ready(function() {
 
-	    function manipulateUI(bCheck) {
-		if (bCheck) {
+			listenerOnCategories()
 
-		    // send data
-		document.getElementById("idSubmitEnquiry").innerHTML = "Submiting..";
+			function manipulateUI(bCheck) {
+				if (bCheck) {
 
-		document.getElementById("idName")
-			.setAttribute("disabled", true)
-		document.getElementById("idAddress").setAttribute("disabled",
-			true)
-		document.getElementById("idEmail").setAttribute("disabled",
-			true)
-		document.getElementById("idPhone").setAttribute("disabled",
-			true)
-		document.getElementById("idMessage").setAttribute("disabled",
-			true)
+					// send data
+				document.getElementById("idSubmitEnquiry").innerHTML = "Submiting..";
 
-	    } else {
+				document.getElementById("idName")
+						.setAttribute("disabled", true)
+				document.getElementById("idAddress").setAttribute("disabled",
+						true)
+				document.getElementById("idEmail").setAttribute("disabled",
+						true)
+				document.getElementById("idPhone").setAttribute("disabled",
+						true)
+				document.getElementById("idMessage").setAttribute("disabled",
+						true)
 
+			} else {
+
+				// send data
+				document.getElementById("idSubmitEnquiry").innerHTML = "Submit";
+
+				document.getElementById("idName").removeAttribute("disabled");
+				document.getElementById("idAddress")
+						.removeAttribute("disabled");
+				document.getElementById("idEmail").removeAttribute("disabled");
+				document.getElementById("idPhone").removeAttribute("disabled");
+				document.getElementById("idMessage")
+						.removeAttribute("disabled");
+
+				document.getElementById("idName").value = "";
+				document.getElementById("idAddress").value = "";
+				document.getElementById("idEmail").value = "";
+				document.getElementById("idPhone").value = "";
+				document.getElementById("idMessage").value = "";
+
+				document.getElementById("idName").style.border = "none";
+				document.getElementById("idEmail").style.border = "none";
+
+			}
+
+		}
+		;
 		// send data
-		document.getElementById("idSubmitEnquiry").innerHTML = "Submit";
+		function sendData(url, methd, obj) {
+			var xmlhttp;
+			if (window.XMLHttpRequest) {// code for IE7+, Firefox,
+				// Chrome,
+				// Opera, Safari
+				xmlhttp = new XMLHttpRequest();
+			} else {// code for IE6, IE5
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+					manipulateUI(false);
+					// document.getElementById("myDiv").innerHTML =
+					// xmlhttp.responseText;
+				} else if (xmlhttp.status == 500 || xmlhttp.status == 404
+						|| xmlhttp.status == 302) {
+					manipulateUI(false);
+				}
 
-		document.getElementById("idName").removeAttribute("disabled");
-		document.getElementById("idAddress")
-			.removeAttribute("disabled");
-		document.getElementById("idEmail").removeAttribute("disabled");
-		document.getElementById("idPhone").removeAttribute("disabled");
-		document.getElementById("idMessage")
-			.removeAttribute("disabled");
+			}
+			xmlhttp.open(methd, url, true);
+			xmlhttp.send(JSON.stringify(obj));
 
-		document.getElementById("idName").value = "";
-		document.getElementById("idAddress").value = "";
-		document.getElementById("idEmail").value = "";
-		document.getElementById("idPhone").value = "";
-		document.getElementById("idMessage").value = "";
-		
-		document.getElementById("idName").style.border = "none";
-		document.getElementById("idEmail").style.border = "none";
-		
-	    }
-
-	}
-	;
-	// send data
-	function sendData(url, methd, obj) {
-	    var xmlhttp;
-	    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome,
-		// Opera, Safari
-		xmlhttp = new XMLHttpRequest();
-	    } else {// code for IE6, IE5
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	    }
-	    xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		    manipulateUI(false);
-		    // document.getElementById("myDiv").innerHTML =
-		    // xmlhttp.responseText;
-		} else if (xmlhttp.status == 500 || xmlhttp.status == 404
-			|| xmlhttp.status == 302) {
-		    manipulateUI(false);
 		}
 
-	    }
-	    xmlhttp.open(methd, url, true);
-	    xmlhttp.send(JSON.stringify(obj));
+		var clickHandler = function() {
+			alert(" clicked");
+
+			var objForServer = {};
+
+			objForServer["name"] = document.getElementById("idName").value;
+			objForServer["address"] = document.getElementById("idAddress").value;
+			objForServer["email"] = document.getElementById("idEmail").value;
+			objForServer["phone"] = document.getElementById("idPhone").value;
+			objForServer["message"] = document.getElementById("idMessage").value;
+
+			// name
+			if (objForServer["name"] == "" || objForServer["name"] == undefined) {
+				document.getElementById("idName").style.border = "3px red solid";
+				return false;
+			}
+
+			// email
+			if (objForServer["email"] == ""
+					|| objForServer["email"] == undefined) {
+				document.getElementById("idEmail").style.border = "3px red solid";
+				return false;
+			}
+			document.getElementById("idMessage").style.border = "none";
+			document.getElementById("idPhone").style.border = "none";
+
+			manipulateUI(true);
+			sendData("LayerInterface", "POST", objForServer);
+
+		};
+
+		// event listener
+		document
+				.getElementById("idSubmitEnquiry")
+				.addEventListener(
+						"click",
+						function() {
+							var sContent = document
+									.getElementById("idSubmitEnquiry").innerHTML;
+							if (sContent !== "Submiting..")
+								clickHandler();
+
+						});
+
+	}	);
+
+/**
+ * 
+ * 
+ * 
+ */
+
+var deviatorUI = {}
+deviatorUI.render = function(id, objData) {
+	var key, baseString = document.getElementById(id).innerHTML;
+	for (key in objData) {
+		baseString = baseString.replace(new RegExp("<%=" + key + "=%>", "g"),
+				objData[key]);
 
 	}
 
-	var clickHandler = function() {
-	    alert(" clicked");
+	return baseString;
+}
 
-	    var objForServer = {};
+// galleryItemTemplate
 
-	    objForServer["name"] = document.getElementById("idName").value;
-	    objForServer["address"] = document.getElementById("idAddress").value;
-	    objForServer["email"] = document.getElementById("idEmail").value;
-	    objForServer["phone"] = document.getElementById("idPhone").value;
-	    objForServer["message"] = document.getElementById("idMessage").value;
+// galleryItemTemplate
+var galleryDataCategory2 = [ {
+	"imgPath" : "img/thumbnails/Thumbnail_211x141_1.jpg",
+	"imgDescription" : "Thumbnail 1",
+	"imgDimension" : "10x10x01x10",
+	"imgPrice" : "2000INR"
+}, {
+	"imgPath" : "img/thumbnails/Thumbnail_211x141_1.jpg",
+	"imgDescription" : "Thumbnail 1",
+	"imgDimension" : "10x10x01x10",
+	"imgPrice" : "2000INR"
+},
 
-	    // name
-	    if (objForServer["name"] == "" || objForServer["name"] == undefined) {
-		document.getElementById("idName").style.border = "3px red solid";
-		return false;
-	    }
+]
+var loadGalleryItems = function(parentId, galleryData) {
+	var strHTML = "";
+	for ( var i = 0; i < galleryData.length; i++) {
+		strHTML += deviatorUI.render("galleryItemTemplate", galleryData[i]);
+	}
+	document.getElementById(parentId).innerHTML = strHTML;
 
-	    // email
-	    if (objForServer["email"] == ""
-		    || objForServer["email"] == undefined) {
-		document.getElementById("idEmail").style.border = "3px red solid";
-		return false;
-	    }
-	    document.getElementById("idMessage").style.border = "none";
-	    document.getElementById("idPhone").style.border = "none";
-		
-		
-		
-	    manipulateUI(true);
-	    sendData("LayerInterface", "POST", objForServer);
+};
 
-	};
+/*
+ * cATEgories
+ */
 
-	// event listener
-	document
-		.getElementById("idSubmitEnquiry")
-		.addEventListener(
-			"click",
-			function() {
-			    var sContent = document
-				    .getElementById("idSubmitEnquiry").innerHTML;
-			    if (sContent !== "Submiting..")
-				clickHandler();
+var listenerOnCategories = function() {
+	var sHTML = "";
+	for ( var i = 0; i < categoryArray.length; i++) {
+		strHTML = deviatorUI.render("categoriesTemplate", categoryArray[i]);
 
-			});
+		var domEle = document.createElement("div");
+		domEle.setAttribute("id", categoryArray[i]["categoryDivName"]);
+		document.getElementById("idGalleryItemsWrapper").appendChild(domEle);
 
-    }   );
+		domEle.style.display = "none";
+
+		$("#accordion").append(strHTML);
+
+		loadGalleryItems(categoryArray[i]["categoryDivName"],
+				categoryArray[i]["categorySource"]);
+
+		document.getElementById(categoryArray[i]["categoryDivName"]).style.display = "none";
+
+		document.getElementById(categoryArray[i]["categoryId"])
+				.addEventListener("click", onCategoryClicked);
+
+	}
+
+	// display 1
+	document.getElementById(categoryArray[0]["categoryDivName"]).style.display = "block";
+
+};
+
+var onCategoryClicked = function(evt) {
+	console.log(evt.target.parentElement.parentElement.parentElement);
+	var str = evt.target.parentElement.parentElement.parentElement.id
+	str = str.replace("cat", "");
+	str = Number(str) - 1;
+	for ( var i = 0; i < categoryArray.length; i++) {
+		if (i === str)
+			document.getElementById(categoryArray[i]["categoryDivName"]).style.display = "block";
+		else
+			document.getElementById(categoryArray[i]["categoryDivName"]).style.display = "none";
+	}
+
+	console.log("ASD>" + str)
+
+}
